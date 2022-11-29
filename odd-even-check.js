24dca3bf198df1e48f36fe2bf6e5a72604f340eb
pixxelshaon <saifulislamshaonapk@gmail.com>
2022-11-29 18:21:12 +0600

    odd-even

---
+++ odd-even-check.js
@@ -0,0 +1,10 @@
+//Check if a number is odd or even in JavaScript
+const isEvenOrOdd = (num) => {
+    if (num%2 === 0){
+      console.log(`${num} is even`);
+    }
+    else{
+      console.log(`${num} is odd`)
+    }
+  }
+  isEvenOrOdd(10);
